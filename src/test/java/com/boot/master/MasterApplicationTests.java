f5928993e6fd99bbe8ade25edd6b36fa39ec8a0b
Java FS <javafs4u@gmail.com>
2023-12-12 19:50:22 +0530

    Initial commit for project

---
+++ src/test/java/com/boot/master/MasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
